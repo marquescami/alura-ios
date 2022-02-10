c509ae9a09cd5658ae3c56dbc47192aff0cb207c
marquescami <camilamsc@hotmail.com>
2022-02-10 18:16:30 -0300

    Criando uma nova celula e registrando na tableView

---
+++ AluraViagens/AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Camila Marques Vasconcelos Loureiro on 10/02/22.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- AluraViagens/AluraViagens/View/ViewController.swift
+++ AluraViagens/AluraViagens/View/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configuraTableView()
+        view.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
+    }
+    
+    func configuraTableView() {
+        viagensTableView.register(UINib(nibName: "ViagemTableViewCell", bundle: nil), forCellReuseIdentifier: "ViagemTableViewCell")
         viagensTableView.dataSource = self
         viagensTableView.delegate = self
-        
-        view.backgroundColor = UIColor(red: 30.0/255.0, green: 59.0/255.0, blue: 119.0/255.0, alpha: 1)
     }
     
 }
@@ -29,11 +33,13 @@ class ViewController: UIViewController {
         }
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
             
-            cell.textLabel?.text = "viagem \(indexPath.row)"
+            guard let celulaViagem = tableView.dequeueReusableCell(withIdentifier: "ViagemTableViewCell") as? ViagemTableViewCell else {
+                fatalError("error to create ViagemTableViewCell")
+                
+            }
             
-            return cell
+            return celulaViagem
         }
     }
 extension ViewController: UITableViewDelegate {
@@ -46,5 +52,10 @@ extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 300
+    }
+    
+    //metodo para indicacao da altura da celula
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 400
     }
 }
